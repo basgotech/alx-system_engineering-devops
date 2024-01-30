fcd1ed866132f10eaf4d6de88ae21bb506c5e2c9
Basliel Tegegn <basgolilo5@gmail.com>
2024-01-30 04:05:00 -0800

    adding

--- 0x06-regular_expressions/4-repetition_token_3.rb
+++ 0x06-regular_expressions/4-repetition_token_3.rb
@@ -1,13 +1,2 @@
 #!/usr/bin/env ruby
-
-# Check if the script has been given an argument
-input_text = ARGV[0]
-
-# Define the regular expression to match "hbn," "hbon," "hbtn," "hbttn," "hbtttn," "hbttttn"
-regex = /\bhbn\b|\bhbon\b|\bhbtn\b|\bhbt{1,5}n\b/
-
-# Scan the input text and join the matched results
-result = input_text.scan(regex).join(' ')
-
-# Print the result
-puts result
+puts ARGV[0].scan(/hbt*n/).join
