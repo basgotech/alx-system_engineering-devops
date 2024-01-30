ad447079b59e7e58a7bd3061bcd2c314029d1b65
Basliel Tegegn <basgolilo5@gmail.com>
2024-01-30 03:11:54 -0800

    adding simle rb

--- 0x06-regular_expressions/0-simply_match_school.rb
+++ 0x06-regular_expressions/0-simply_match_school.rb
@@ -1,20 +1,2 @@
 #!/usr/bin/env ruby
-
-# Check if the script has been given an argument
-if ARGV.empty?
-  puts "Usage: #{$0} <text>"
-  exit 1
-end
-
-# Get the input text from the command line argument
-input_text = ARGV[0]
-
-# Define the regular expression to match "School"
-regex = /\bSchool\b/
-
-# Check if the input text matches the regular expression
-if input_text.match?(regex)
-  puts "#{input_text}$"
-else
-  puts "$"
-end
+puts ARGV[0].scan(/School/).join
