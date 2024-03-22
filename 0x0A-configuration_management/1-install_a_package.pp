0d4a163203fe7fa2fab19885c69633cc8cdfc923
Basliel Tegegn <basgolilo5@gmail.com>
2024-03-22 12:32:30 -0700

    ad"

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,9 +1,7 @@
 #!/usr/bin/pup
-# Define a resource class for managing pip packages
-resource class pip::package { }
 
 # Install Flask 2.1.0 using pip3
-pip::package { 'Flask':
+package { 'Flask':
   ensure   => '2.1.0',
   provider => 'pip3',
 }
